ccc365c34f6c1a26ad77b92ca7a08c83401706b0
Taddeivitor <taddeivitor@hotmail.com>
2020-03-13 16:00:56 -0300

    Ajustes no codigo

--- app-challenges/Begginer/BorderRadiusPreviewer/main.js
+++ app-challenges/Begginer/BorderRadiusPreviewer/main.js
@@ -1,9 +1,9 @@
 function changeBorder() {
     const box = document.getElementById('box')
-    const topRight = document.getElementById('menu-top-right').value + "px"
-    const topLeft = document.getElementById('menu-top-left').value + "px"
-    const bottomRight = document.getElementById('menu-bottom-right').value + "px"
-    const bottomLeft = document.getElementById('menu-bottom-left').value + "px"
+    const topRight = `${document.getElementById('menu-top-right').value}px`
+    const topLeft = `${document.getElementById('menu-top-left').value}px`
+    const bottomRight = `${document.getElementById('menu-bottom-right').value}px`
+    const bottomLeft = `${document.getElementById('menu-bottom-left').value}px`
 
     box.style.borderTopRightRadius = topRight
     box.style.borderTopLeftRadius = topLeft
@@ -20,15 +20,15 @@ function copyToClipboard() {
     const botL = box.style.borderBottomLeftRadius
 
     if (topR && topL && botR == botL) {
-        var borderRadius = "border-radius: " + botL + ";"
+        var borderRadius = `border-radius: ${botL};`
         document.getElementById('code').value = borderRadius
     } else {
-        var borderTopRight = "border-top-right-radius: " + topR + ";"
-        var borderTopLeft = "border-top-left-radius: " + topL + ";"
-        var borderBottomRight = "border-bottom-right-radius: " + botR + ";"
-        var borderBottomLeft = "border-bottom-left-radius: " + botL + ";"
-        if (topR && topL && botR && botL !== ""){
-        document.getElementById('code').value = borderTopRight + "\n" + borderTopLeft + "\n" + borderBottomRight + "\n" + borderBottomLeft
+        var borderTopRight = `border-top-right-radius: ${topR};`
+        var borderTopLeft = `border-top-left-radius: ${topL};`
+        var borderBottomRight = `border-bottom-right-radius: ${botR};`
+        var borderBottomLeft = `border-bottom-left-radius: ${botL};`
+        if (topR !== "" && topL !== "" && botR !== "" && botL !== "") {
+            document.getElementById('code').value = `${borderTopRight}\n${borderTopLeft}\n${borderBottomRight}\n${borderBottomLeft}`
         } else {
             document.getElementById('code').value = "Bordas não possuem valores"
         }
