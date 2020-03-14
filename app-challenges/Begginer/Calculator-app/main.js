1404030e741433a034e32279c85166961a13d0c4
Taddeivitor <taddeivitor@hotmail.com>
2020-03-14 18:31:55 -0300

    Functions Add

--- app-challenges/Begginer/Calculator-app/main.js
+++ app-challenges/Begginer/Calculator-app/main.js
@@ -6,13 +6,134 @@ function onlynumber() {
     }
 }
 
-function catchNumber(input) {
+function catchNumber(input) { //coleta o valor do input dos numeros
     var inputValue = input.value
-    var currentValue = document.getElementById('current-num')
+    var currentNum = document.getElementById('current-num')
 
-    if (currentValue.value.length < 8) {
-       currentValue.value += inputValue
+    if (currentNum.value.length < 8) {
+        currentNum.value += inputValue
     } else return false
 
-    return valor
+}
+
+function catchOperation(input) { //coleta o valor do input das operações de limpar e limpar tudo e executa
+    var inputValue = input.value
+    var matSymbol = document.getElementById('mat-symbol')
+    var currentNum = document.getElementById('current-num')
+    var lastNum = document.getElementById('last-num')
+    var matResult = document.getElementById('mat-result')
+
+    var operation = inputValue
+
+    switch (operation) {
+        case "CE":
+            clear()
+            break
+        case "C":
+            clearAll()
+            break
+    }
+
+    function clear() {
+        currentNum.value = ""
+    }
+    function clearAll() {
+        currentNum.value = ""
+        lastNum.value = ""
+        matSymbol.value = ""
+        matResult.value = ""
+    }
+}
+
+function catchMatSymbol(input) { //coleta o valor do input dos simbolos aritméticos
+    var inputValue = input.value
+    var matSymbol = document.getElementById('mat-symbol')
+    var currentNum = document.getElementById('current-num')
+    var lastNum = document.getElementById('last-num')
+    var matResult = document.getElementById('mat-result')
+
+    op()
+    matSymbol.value = inputValue
+
+    function op() { //executa a operação aritmética de acordo com o valor do simbolo coletado
+
+        if(currentNum.value == ""){
+            currentNum.value = 0
+        }
+        else if (lastNum.value == "") {
+            lastNum.value = `${currentNum.value}${matSymbol.value}`
+            matResult.value = currentNum.value
+            currentNum.value = ""
+        } else {
+            switch (matSymbol.value) {
+                case "+":
+                    sum()
+                    calcHist()
+                    break
+                case "-":
+                    sub()
+                    calcHist()
+                    break
+                case "*":
+                    plus()
+                    calcHist()
+                    break
+                case "/":
+                    split()
+                    calcHist()
+                    break
+                case "=":
+                    equal()
+                    break
+            }
+        }
+
+        function equal() { // realiza a ultima operação digitada
+            switch (matSymbol.value) {
+                case "+":
+                    sum()
+                    break
+                case "-":
+                    sub()
+                    break
+                case "*":
+                    plus()
+                    break
+                case "/":
+                    split()
+                    break
+            }
+        }
+
+        function calcHist() { //adiciona o simbolo aritmético + o valor digitado no historico
+            lastNum.value += `${matSymbol.value}${currentNum.value}`
+            currentNum.value = ""
+        }
+        function sum() {
+
+            var firstNum = parseInt(matResult.value, 10)
+            var secondNum = parseInt(currentNum.value, 10)
+            matResult.value = firstNum + secondNum
+        }
+
+        function sub() {
+
+            var firstNum = parseInt(matResult.value, 10)
+            var secondNum = parseInt(currentNum.value, 10)
+            matResult.value = firstNum - secondNum
+        }
+
+        function plus() {
+
+            var firstNum = parseInt(matResult.value, 10)
+            var secondNum = parseInt(currentNum.value, 10)
+            matResult.value = firstNum * secondNum
+        }
+        function split() {
+
+            var firstNum = parseInt(matResult.value, 10)
+            var secondNum = parseInt(currentNum.value, 10)
+            matResult.value = firstNum / secondNum
+        }
+    }
 }
